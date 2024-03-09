4e7ffcad446d34aa53a1c3f3ca78d7fb23902667
German <gogapopp@gmail.com>
2024-03-09 18:19:28 +0300

    implementing blocks with protobuffers

---
+++ types/block.go
@@ -0,0 +1,23 @@
+package types
+
+import (
+	"crypto/sha256"
+
+	"github.com/gogapopp/blockchain/crypto"
+	"github.com/gogapopp/blockchain/proto"
+	pb "google.golang.org/protobuf/proto"
+)
+
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
+
+// HashBlock creates a SHA256 of the header
+func HashBlock(block *proto.Block) []byte {
+	b, err := pb.Marshal(block)
+	if err != nil {
+		panic(err)
+	}
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
