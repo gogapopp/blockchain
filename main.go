ca6672cecf90be7f6a6f22f97f25f10bdfbaa6da
German <gogapopp@gmail.com>
2024-03-12 22:05:12 +0300

    add mempool

--- main.go
+++ main.go
@@ -1,47 +1,69 @@
 package main
 
 import (
+	"context"
+	"log"
 	"time"
 
+	"github.com/gogapopp/blockchain/crypto"
 	"github.com/gogapopp/blockchain/node"
+	"github.com/gogapopp/blockchain/proto"
+	"github.com/gogapopp/blockchain/util"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	makeNode(":3000", []string{})
-	time.Sleep(time.Second * 1)
-	makeNode(":4000", []string{":3000"})
-	time.Sleep(time.Second * 1)
-	makeNode(":5000", []string{":4000"})
+	makeNode(":3000", []string{}, true)
+	time.Sleep(time.Second)
+	makeNode(":4000", []string{":3000"}, false)
+	time.Sleep(time.Second)
+	makeNode(":5000", []string{":4000"}, false)
 
-	// go func() {
-	// 	for {
-	// 		time.Sleep(2 * time.Second)
-	// 		makeTransaction()
-	// 	}
-	// }()
-	select {}
+	for {
+		time.Sleep(time.Second * 2)
+		makeTransaction()
+	}
 }
 
-func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
-	n := node.NewNode()
+func makeNode(listenAddr string, bootstrapNodes []string, isValidator bool) *node.Node {
+	cfg := node.ServerConfig{
+		Version:    "blockchain-0.1",
+		ListenAddr: listenAddr,
+	}
+	if isValidator {
+		cfg.PrivateKey = crypto.GeneratePrivateKey()
+	}
+	n := node.NewNode(cfg)
 	go n.Start(listenAddr, bootstrapNodes)
 	return n
 }
 
-// func makeTransaction() {
-// 	client, err := grpc.Dial(":3000", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	c := proto.NewNodeClient(client)
+func makeTransaction() {
+	client, err := grpc.Dial(":3000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := proto.NewNodeClient(client)
+	privKey := crypto.GeneratePrivateKey()
+	tx := &proto.Transaction{
+		Version: 1,
+		Inputs: []*proto.TxInput{
+			{
+				PrevTxHash:   util.RandomHash(),
+				PrevOutIndex: 0,
+				PublicKey:    privKey.Public().Bytes(),
+			},
+		},
+		Outputs: []*proto.TxOutput{
+			{
+				Amount:  99,
+				Address: privKey.Public().Address().Bytes(),
+			},
+		},
+	}
 
-// 	version := &proto.Version{
-// 		Version:    "blockchain-0.1",
-// 		Height:     1,
-// 		ListenAddr: ":4000",
-// 	}
-// 	_, err = c.Handshake(context.TODO(), version)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+	_, err = c.HandleTransaction(context.TODO(), tx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
